5ba9413dc5e6caba57fcd658b2b264e3406addee
Ritchie Nithoo <ritchie.nithoo@gmail.com>
2025-01-08 09:42:31 +0100

    Initial commit of all files

---
+++ src/test/java/com/cnd13natation/records/ClubRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnd13natation.records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
